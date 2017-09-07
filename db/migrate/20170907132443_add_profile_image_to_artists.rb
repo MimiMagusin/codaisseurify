18c380dd562dc6aaaa9917e69c69340127161bec
MimiMagusin <mimi_magusin@hotmail.com>
2017-09-07 15:35:25 +0200

    test and create index with names artists

---
+++ db/migrate/20170907132443_add_profile_image_to_artists.rb
@@ -0,0 +1,5 @@
+class AddProfileImageToArtists < ActiveRecord::Migration[5.1]
+  def change
+    add_column :artists, :profile_picture, :string
+  end
+end
---
+++ spec/features/list_artists_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "Current user viewing the list of artists" do
+
+
+
+  let!(:artist1) { create :artist, first_name: "Listing Name 1" }
+  let!(:artist2) { create :artist, first_name: "Listing Name 2" }
+  let!(:artist3) { create :artist, first_name: "Another users artist" }
+
+  it "shows all his artists" do
+    visit artists_url
+
+    expect(page).to have_text("Listing Name 1")
+    expect(page).to have_text("Listing Name 2")
+  end
+end
