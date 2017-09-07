b7f9d0d8743e271acdd76daa4fa7786a4f7e971b
MimiMagusin <mimi_magusin@hotmail.com>
2017-09-07 15:55:00 +0200

    Add picture to artist index and test

--- spec/features/list_artists_spec.rb
+++ spec/features/list_artists_spec.rb
@@ -4,7 +4,7 @@ describe "Current user viewing the list of artists" do
 
 
 
-  let!(:artist1) { create :artist, first_name: "Listing Name 1" }
+  let!(:artist1) { create :artist, first_name: "Listing Name 1", profile_picture: "I am beautiful!" }
   let!(:artist2) { create :artist, first_name: "Listing Name 2" }
   let!(:artist3) { create :artist, first_name: "Another users artist" }
 
@@ -12,6 +12,7 @@ describe "Current user viewing the list of artists" do
     visit artists_url
 
     expect(page).to have_text("Listing Name 1")
+    expect(page).to have_text("I am beautiful!")
     expect(page).to have_text("Listing Name 2")
   end
 end
