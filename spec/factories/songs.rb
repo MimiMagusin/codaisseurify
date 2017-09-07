240779ba28dfd540f4ad809ae4ec77b239fb73a9
MimiMagusin <mimi_magusin@hotmail.com>
2017-09-07 18:15:39 +0200

    Create model of song and migrate

--- spec/factories/songs.rb
+++ spec/factories/songs.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :song do
-        first_name  { Faker::Name.title }
-        last_name   { Faker::Integer.release_year }
-      end
+    title "MyString"
+    release_year 1
+  end
 end
