8f8993be131ee964ec9bfacceb3afecf2a46bb25
MimiMagusin <mimi_magusin@hotmail.com>
2017-09-07 18:24:08 +0200

    Associate one artist to many songs

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,6 @@
 class Artist < ApplicationRecord
+  has_many :songs
+
   def full_name
     return "#{first_name} #{last_name}"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907161335) do
+ActiveRecord::Schema.define(version: 20170907162253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20170907161335) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_picture"
+    t.bigint "song_id"
+    t.index ["song_id"], name: "index_artists_on_song_id"
   end
 
   create_table "songs", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema.define(version: 20170907161335) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "artists", "songs"
 end
