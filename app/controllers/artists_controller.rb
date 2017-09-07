1a23217a935dbf1b74060c51054ba7906e907aad
MimiMagusin <mimi_magusin@hotmail.com>
2017-09-07 17:29:26 +0200

    Change artist full names on index into links and test

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,4 +2,6 @@ class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
   end
+
+  def show;  end
 end
--- spec/features/list_artists_spec.rb
+++ spec/features/list_artists_spec.rb
@@ -1,28 +1,27 @@
 require 'rails_helper'
-
-describe "Visitor viewing the list of artists" do
-
-
-
+describe "Artist Views" do
   let!(:artist1) { create :artist, first_name: "Listing Name 1", last_name: "Lastname", profile_picture: "I am beautiful!" }
   let!(:artist2) { create :artist, first_name: "Listing Name 2", last_name: "Lastname" }
   let!(:artist3) { create :artist, first_name: "Another users artist" }
 
-  it "shows all his artists" do
-    visit artists_url
+  describe "Visitor viewing the list of artists" do
 
-    expect(page).to have_text("Listing Name 1 Lastname")
-    expect(page).to have_text("I am beautiful!")
-    expect(page).to have_text("Listing Name 2")
+    it "shows all his artists" do
+      visit artists_url
+
+      expect(page).to have_text("Listing Name 1 Lastname")
+      expect(page).to have_text("I am beautiful!")
+      expect(page).to have_text("Listing Name 2")
+    end
+  end
+
+  describe "Visitor clicking on names" do
+    it "redirect to artist show page" do
+      visit artists_url
+
+      click_on artist1.full_name
+
+      expect(page).to have_content "This application was built in 2017"
+    end
   end
 end
-
-#describe "Visitor clicking on names" do
-  #it "redirect to show page" do
-  #  visit artists_url
-
-    #click_on artist.full_name
-
-  #  expect(page).to have_content "This application was built in 2017"
-  #end
-#end
