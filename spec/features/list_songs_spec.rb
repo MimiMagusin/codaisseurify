267f93e92e1443bab3adad400912e09e72bfb0c6
MimiMagusin <mimi_magusin@hotmail.com>
2017-09-08 17:07:12 +0200

    Fix list_song test

--- spec/features/list_songs_spec.rb
+++ spec/features/list_songs_spec.rb
@@ -1,26 +1,22 @@
 require 'rails_helper'
 
-Song.destroy_all
-Artist.destroy_all
-
 describe "Artist Show" do
-  let!(:song1) {create :song, title: "Ik klom eens op een heg", release_year: 1996, artist: artist1}
-  let!(:song2) {create :song, title: "Ik klom eens op een andere heg", release_year: 1997, artist: artist1}
+  let!(:artist) { create :artist, first_name: "Listing Name 1", last_name: "Lastname", profile_picture: "I am beautiful!" }
 
-  let!(:artist1) { create :artist, first_name: "Listing Name 1", last_name: "Lastname", profile_picture: "I am beautiful!" }
+  let!(:song1) {create :song, title: "Ik klom eens op een heg", release_year: 1996, artist: artist}
+  let!(:song2) {create :song, title: "Ik klom eens op een andere heg", release_year: 1997, artist: artist}
 
   describe "Visitor viewing the artist show page" do
 
     it "shows all songs of artist" do
-      visit artists_path(:id)
+      visit artist_path(artist.id)
 
       expect(page).to have_text("Ik klom eens op een heg")
       expect(page).to have_text("Ik klom eens op een andere heg")
       expect(page).to have_text("Listing Name 1 Lastname")
       expect(page).to have_text("I am beautiful!")
-      expect(page).to have_integer(1996)
-      expect(page).to have_integer(1997)
-
+      expect(page).to have_text(1996)
+      expect(page).to have_text(1997)
     end
   end
 end
