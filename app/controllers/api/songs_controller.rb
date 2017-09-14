c6c5bec1ea360567778decbaaa85ddf55279eb24
MimiMagusin <mimi_magusin@hotmail.com>
2017-09-14 17:27:52 +0200

    Update API songs create and destroy actions

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,6 +1,33 @@
 class Api::SongsController < ApplicationController
-  def create; end
+  def create
+    song = Song.new(song_params)
 
-  def destroy;
+    if song.save
+      render status: 201, json: {
+        message: "song successfully created",
+        song: song
+      }.to_json
+    else
+      render status: 422, json: {
+        errors: song.errors
+      }.to_json
+    end
   end
+
+  def destroy
+    song = Song.find(params[:id])
+    song.destroy
+
+    render status: 200, json: {
+      message: "song successfully deleted"
+    }.to_json
+  end
+
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :release_year, :artist_id
+  end
+end
 end
