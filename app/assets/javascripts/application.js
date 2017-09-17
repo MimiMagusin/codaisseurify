371e6ddd2e1f154486ab52000065ae74427fa923
MimiMagusin <mimi_magusin@hotmail.com>
2017-09-17 12:12:35 +0200

    Delete all songs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,42 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require_tree .
+function submitSong(event) {
+
+  function createSong(title) {
+
+    var listItem = $('<li></li>');
+    listItem.addClass("song");
+
+    var label = $('<label></label>');
+    label.attr('for', checkboxId);
+    label.html(title);
+
+
+    listItem.append(label);
+
+    $("#songlist").append( listItem );
+  }
+
+function nextSongId() {
+  return $(".song").length + 1;
+}
+    // stop the form from doing the default action, submitting...
+    event.preventDefault();
+
+    var title = $("#new-song").val();
+
+    createSong(title);
+
+    $("#new-song").val(null);
+  }
+
+function cleanUpDoneSongs(event) {
+  event.preventDefault();
+  $.when($(".song").remove())
+}
+
+$(document).ready(function() {
+  $("form").bind('submit', submitSong);
+  $("#clean-up").bind('click', cleanUpDoneSongs);
+});
