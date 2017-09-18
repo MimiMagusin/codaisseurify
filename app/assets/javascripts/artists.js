1b9dfbdd72555824b37ac252f261632acfb1a34b
MimiMagusin <mimi_magusin@hotmail.com>
2017-09-18 19:42:10 +0200

    Delete artist via ajax

--- app/assets/javascripts/artists.js
+++ app/assets/javascripts/artists.js
@@ -1,1 +1,17 @@
+//= require jquery
+//= require bootstrap-sprockets
+//= require jquery_ujs
+//= require_tree .
 
+function deleteArtist(ArtistId) {
+  $.ajax({
+    type: "DELETE",
+    url: "/artists/" + artistId + ".json",
+    contentType: "application/json",
+    dataType: "json"
+  })
+  .done(function(data) {
+    $('tr[data-id="'+artistId+'"]').remove();
+    updateCounters();
+  });
+}
